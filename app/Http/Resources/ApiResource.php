9ee743766d71b52f4671a35f50245eb3cac8453e
Anuraaaa <anwarfauzan75@gmail.com>
2024-06-05 10:58:58 +0700

    feat(resources): add API Resource

---
+++ app/Http/Resources/ApiResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ApiResource extends JsonResource
+{
+
+    public $code;
+    public $status;
+    public $message;
+    public $resource;
+
+    public function __construct($code, $status, $message, $resource)
+    {
+        $this->code = $code;
+        $this->status = $status;
+        $this->message = $message;
+        $this->resource = $resource;
+    }
+
+    public function toArray(Request $request): array
+    {
+        return [
+            'code' => $this->code,
+            'success' => $this->status,
+            'message' => $this->message,
+            'resource' => $this->resource,
+        ];    
+    }
+}
